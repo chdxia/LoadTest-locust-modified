19d04aa6d9bec1030a33eb001086557532a751fe
xiachuandong <chdxia@liangzhicn.com>
2020-12-03 17:37:07 +0800

    add test demo

--- locustfile.py
+++ locustfile.py
@@ -1,2 +1,29 @@
-from locust import HttpLocust, TaskSet
+from locust import HttpUser, TaskSet, task
 
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        self.login()
+
+    def on_stop(self):
+        self.logout()
+
+    def login(self):
+        self.client.post("/sessions", {"account": "admin", "password": "888888"})
+
+    def logout(self):
+        self.client.delete("/sessions/1")
+
+    @task(2)
+    def warehouses(self):
+        self.client.get("/warehouses")
+
+    @task(1)
+    def shifts(self):
+        self.client.get("/shifts")
+
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    min_wait = 5000
+    max_wait = 9000
